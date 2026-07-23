9f9b33295de5de2ecacbc925245117be976095ab
Jin Hong Pang <jpan0069@student.monash.edu>
2026-07-23 11:39:16 +1000

    Add new migration for user sessions and update build script to include migrations

--- migrations/index.ts
+++ migrations/index.ts
@@ -2,6 +2,7 @@ import * as migration_20260722_082958_initial from './20260722_082958_initial';
 import * as migration_20260722_093931_add_media from './20260722_093931_add_media';
 import * as migration_20260722_095429_add_committee from './20260722_095429_add_committee';
 import * as migration_20260722_100506_add_sponsors from './20260722_100506_add_sponsors';
+import * as migration_20260723_013827 from './20260723_013827';
 
 export const migrations = [
   {
@@ -22,6 +23,11 @@ export const migrations = [
   {
     up: migration_20260722_100506_add_sponsors.up,
     down: migration_20260722_100506_add_sponsors.down,
-    name: '20260722_100506_add_sponsors'
+    name: '20260722_100506_add_sponsors',
+  },
+  {
+    up: migration_20260723_013827.up,
+    down: migration_20260723_013827.down,
+    name: '20260723_013827'
   },
 ];
